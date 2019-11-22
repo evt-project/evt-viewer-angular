397f867f086684fc69005e0086b30a4b2eb7a1d5
ChiaraDipi <dipi.chiara@gmail.com>
2019-11-22 08:36:08 +0100

    Remove useless code and format file

--- src/app/utils/xmlUtils.ts
+++ src/app/utils/xmlUtils.ts
@@ -1,17 +1,15 @@
-import { Injectable } from '@angular/core';
-
 declare var window: any;
 
 export function parseXml(xmlStr: string): HTMLElement {
-    if (typeof window.DOMParser !== 'undefined') {
-        return (new window.DOMParser()).parseFromString(xmlStr, 'text/xml');
-    } else if (typeof window.ActiveXObject !== 'undefined' &&
-        new window.ActiveXObject('Microsoft.XMLDOM')) {
-        const xmlDoc = new window.ActiveXObject('Microsoft.XMLDOM');
-        xmlDoc.async = 'false';
-        xmlDoc.loadXML(xmlStr);
-        return xmlDoc;
-    } else {
-        throw new Error('No XML parser found');
-    }
+  if (typeof window.DOMParser !== 'undefined') {
+    return (new window.DOMParser()).parseFromString(xmlStr, 'text/xml');
+  } else if (typeof window.ActiveXObject !== 'undefined' &&
+    new window.ActiveXObject('Microsoft.XMLDOM')) {
+    const xmlDoc = new window.ActiveXObject('Microsoft.XMLDOM');
+    xmlDoc.async = 'false';
+    xmlDoc.loadXML(xmlStr);
+    return xmlDoc;
+  } else {
+    throw new Error('No XML parser found');
+  }
 }
