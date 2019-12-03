014b30b62f58c299eceb6b362b472bae4e40e5f8
Memmo91 <dalischi@hotmail.it>
2019-12-03 10:38:30 +0100

    Update citations matching

--- src/app/services/xml-parsers/bibliography-parser.service.ts
+++ src/app/services/xml-parsers/bibliography-parser.service.ts
@@ -19,9 +19,19 @@ export class BibliographyParserService {
     this.editionUrls.forEach((path) => {
       this.http.get(path, { responseType: 'text' }).subscribe((response) => {
         Array.from(parser.parseFromString(response, 'text/xml').getElementsByTagName('bibl')).forEach(citation => {
-          if (!this.bibliographicCitations.includes(citation.textContent)) {
-            console.log(citation.textContent);
+          if (citation.getElementsByTagName('author').length === 0 &&
+              citation.getElementsByTagName('title').length === 0 &&
+              citation.getElementsByTagName('date').length === 0) {
+            // unmarked citation
+            if (!this.bibliographicCitations.includes(citation.textContent)) {
+              console.log(citation.textContent);
+            }
+          } else {
+            // TODO marked citation
           }
+          /* if (!this.bibliographicCitations.includes(citation.textContent)) {
+
+          }*/
           this.bibliographicCitations.push(citation.textContent);
         });
       });
