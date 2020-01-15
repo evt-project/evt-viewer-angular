362d0ee7112319a99d5234e1d8bf7c317c62e66f
ChiaraDipi <dipi.chiara@gmail.com>
2020-01-15 18:23:51 +0100

    Switch to RegExp in filter pipe

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -5,11 +5,18 @@ import { Injectable, Pipe, PipeTransform } from '@angular/core';
 })
 @Injectable()
 export class FilterPipe implements PipeTransform {
-    transform<T>(items: T[], field: string, value: string): T[] {
+    transform<T>(items: T[], field: string, value: string, caseSensitive: boolean): T[] {
         if (!items) { return []; }
         if (!value || value.length === 0) { return items; }
-        const results = items.filter(it => it[field] && value && it[field].toLowerCase().indexOf(value.toLocaleLowerCase()) !== -1);
+        let reg;
+        try {
+            reg = new RegExp(value, `${caseSensitive ? 'i' : ''}g`);
+            const results = items.filter(it => it[field] && value && it[field].match(reg));
 
-        return results.length > 0 ? results : [undefined];
+            return results.length > 0 ? results : [undefined];
+        } catch (e) {
+            return [];
+        }
+
     }
 }
