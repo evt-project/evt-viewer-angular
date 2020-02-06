f074bfdddcf816ac3c97c32873fd0d869da4c1c0
ChiaraDipi <dipi.chiara@gmail.com>
2020-02-07 00:09:38 +0100

    Set ref highlight color depending on configuration

--- src/app/components/named-entity-ref/named-entity-ref.component.ts
+++ src/app/components/named-entity-ref/named-entity-ref.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { map } from 'rxjs/operators';
+import { AppConfig } from '../../app.config';
 import { NamedEntityRefData } from '../../models/evt-models';
 import { register } from '../../services/component-register.service';
 import { EntitiesSelectService } from '../../services/entities-select.service';
 import { NamedEntitiesParserService } from '../../services/xml-parsers/named-entities-parser.service';
+import { EntitiesSelectItem } from '../entities-select/entities-select.component';
 
 @Component({
   selector: 'evt-named-entity-ref',
@@ -11,7 +13,7 @@ import { NamedEntitiesParserService } from '../../services/xml-parsers/named-ent
   styleUrls: ['./named-entity-ref.component.scss'],
 })
 @register
-export class NamedEntityRefComponent {
+export class NamedEntityRefComponent implements OnInit {
   @Input() data: NamedEntityRefData;
 
   entity$ = this.neParserService.namedEntities$.pipe(
@@ -21,6 +23,9 @@ export class NamedEntityRefComponent {
   public highlighted$ = this.entitiesSelectService.selectedItems$.pipe(
     map(items => items.some(i => i && this.matchEntitySelection(i.value))),
   );
+
+  public highlightColor;
+
   public opened = false;
 
   constructor(
@@ -29,9 +34,41 @@ export class NamedEntityRefComponent {
   ) {
   }
 
+  ngOnInit() {
+    this.setHighlightColor();
+  }
+
   toggleEntityData(event: MouseEvent) {
     event.stopPropagation();
     this.opened = !this.opened;
+  }
+
+  private setHighlightColor() {
+    const entitiesSelectItems = AppConfig.evtSettings.ui.entitiesSelectItems
+      .reduce((i: EntitiesSelectItem[], g) => i.concat(g.items), [])
+      .reduce((x: EntitiesSelectItem[], y) => {
+        const multiValues: EntitiesSelectItem[] = [];
+        y.value.split(',').forEach(t => {
+          multiValues.push({ ...y, value: t });
+        });
+
+        return x.concat(multiValues);
+      },      []);
+
+    let bestMatch: EntitiesSelectItem & { score: number };
+    entitiesSelectItems.forEach(item => {
+      let score = 0;
+      score += this.matchEntitySelectionClass(item.value) ? 1 : 0;
+      const attributes = this.entitiesSelectService.getAttributesFromValue(item.value);
+      score += attributes.length && this.matchEntitySelectionAttributes(item.value) ? 1 : 0;
+      if (!bestMatch || bestMatch.score < score) {
+        bestMatch = {
+          ...item,
+          score,
+        };
+      }
+    });
+    this.highlightColor = bestMatch ? bestMatch.color : '';
   }
 
   private matchEntitySelection(selectedValue: string) {
