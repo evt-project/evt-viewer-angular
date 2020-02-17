0f7b842dcfe766e85b0f8bbaab0b00693437647a
Memmo91 <dalischi@hotmail.it>
2020-02-17 09:45:33 +0100

    Update bibliography component

--- src/app/components/bibliography/bibliography.component.ts
+++ src/app/components/bibliography/bibliography.component.ts
@@ -9,155 +9,100 @@ import { BibliographicCitation, BibliographyParserService } from '../../services
   styleUrls: ['./bibliography.component.scss'],
 })
 export class BibliographyComponent implements OnDestroy {
-
   public biblCits: BibliographicCitation[] = [];
-  private subscription: Subscription;
+  private subscriptions: Subscription[] = [];
   public isComplete: boolean;
-  selectedAlphOrder: 'Ascendent';
-  selectedField: 'Author';
-  selectedStyle: 'Chicago';
-  features: string[] = ['Author', 'Title', 'Date'];
-  styles: string[] = ['Chicago', 'APA'];
-  alphOrders: string[] = ['Ascendent', 'Descendent'];
+  public styles = ['Chicago', 'APA'];
+  private sortingFields;
+  public selectedStyle: string;
+  public selectedField: string;
+  public selectedAlphOrder: string;
 
   constructor(
-    public bps: BibliographyParserService,
+    public biblParsService: BibliographyParserService,
     public http: HttpClient,
   ) {
-
-    this.subscription = this.bps.getBibliographicCitations().subscribe(response => {
+    this.subscriptions.push(this.biblParsService.getBibliographicCitations().subscribe(response => {
       this.biblCits = response.citations;
       this.isComplete = response.areComplete;
-    });
-    this.swap();
-    this.isComplete
-      ? this.sortBy('Ascendent', this.selectedField)
-      : this.sortBy('Ascendent');
+    }));
+    if (this.isComplete) {
+      this.sortingFields = {};
+      this.mapSortingField('assets/i18n/it.json');
+      this.mapSortingField('assets/i18n/en.json');
+    }
   }
 
-  swap() {
-    const biblPars = document.getElementsByClassName('biblPar');
-
-    const authors = document.getElementsByClassName('authors');
-    const dates = document.getElementsByClassName('dates');
-    const titles = document.getElementsByClassName('titles');
-    const editors = document.getElementsByClassName('editors');
-    const pubPlaces = document.getElementsByClassName('pubPlaces');
-    const publishers = document.getElementsByClassName('publishers');
-    const biblScopes = document.getElementsByClassName('biblScopes');
-    const series = document.getElementsByClassName('series');
-
-    const analyticAuthors = document.getElementsByClassName('analytic_author');
-    const analyticTitles = document.getElementsByClassName('analytic_titles');
-    const monogrAuthors = document.getElementsByClassName('monogr_authors');
-    const monogrTitles = document.getElementsByClassName('monogr_titles');
-    const monogrEditors = document.getElementsByClassName('monogr_editors');
-    const monogrImprintPubPlaces = document.getElementsByClassName('monogr_imprint_pubPlaces');
-    const monogrImprintPublishers = document.getElementsByClassName('monogr_imprint_publishers');
-    const monogrImprintDates = document.getElementsByClassName('monogr_imprint_dates');
-    const monogrBiblScopes = document.getElementsByClassName('monogr_biblScopes');
-    const seriesTitles = document.getElementsByClassName('series_titles');
-    const seriesBiblScopes = document.getElementsByClassName('series_biblScopes');
-
-    const notes = document.getElementsByClassName('notes');
-
-    const ps = Array.from(biblPars);
-    let a: HTMLCollection[];
-    if (this.selectedStyle === 'Chicago') {
-      a = [
-        authors,
-        analyticAuthors,
-        dates,
-        titles,
-        analyticTitles,
-        monogrTitles,
-        monogrAuthors,
-        monogrTitles,
-        monogrEditors,
-        monogrBiblScopes,
-        seriesTitles,
-        seriesBiblScopes,
-        series,
-        notes,
-        editors,
-        pubPlaces,
-        publishers,
-        biblScopes];
-    } else {
-      a = [
-        authors,
-        analyticAuthors,
-        dates,
-        titles,
-        analyticTitles,
-        monogrTitles,
-        monogrAuthors,
-        monogrTitles,
-        monogrImprintPubPlaces,
-        monogrImprintPublishers,
-        monogrImprintDates,
-        monogrEditors,
-        monogrBiblScopes,
-        editors,
-        pubPlaces,
-        publishers,
-        biblScopes,
-        seriesTitles,
-        seriesBiblScopes,
-        series,
-        notes];
-    }
-    for (let i = 0; i < ps.length; i++) {
-      a.forEach(tag => {
-        if (tag[i] !== undefined) {
-          ps[i].appendChild(tag[i]);
+  private mapSortingField(path: string) {
+    this.subscriptions.push(this.http.get(path).subscribe(response => {
+      Object.keys(response).forEach(key => {
+        if (key === 'author') {
+          Object.keys(this.biblCits[0]).includes('analyticAuthor')
+            ? this.sortingFields[response[key]] = 'analyticAuthor'
+            : this.sortingFields[response[key]] = 'author';
+        } else if (key === 'title') {
+          Object.keys(this.biblCits[0]).includes('analyticTitle')
+            ? this.sortingFields[response[key]] = 'analyticTitle'
+            : this.sortingFields[response[key]] = 'title';
+        } else if (key === 'date') {
+          Object.keys(this.biblCits[0]).includes('monogrImprintDate')
+            ? this.sortingFields[response[key]] = 'monogrImprintDate'
+            : this.sortingFields[response[key]] = 'date';
         }
       });
-    }
+    }));
   }
 
-  sortBy(mode: 'Ascendent' | 'Descendent', field?: string): BibliographicCitation[] {
+  private swapFields(orderStyle: string[]) {
+    const biblPars = Array.from(document.getElementsByClassName('biblPar'));
+    biblPars.forEach(biblPar => {
+      orderStyle.concat(this.biblParsService.biblTagChilds).concat(this.biblParsService.biblStructTagChilds).forEach(tagClass => {
+        Array.from(biblPar.getElementsByClassName(tagClass)).forEach(tagClassElement => {
+          if (tagClassElement.parentNode === biblPar) {
+            biblPar.appendChild(tagClassElement);
+          }
+        });
+      });
+    });
+  }
+
+  swapBiblCit(selectedStyle: string) {
+    selectedStyle === 'Chicago'
+      ? this.swapFields(['author', 'analyticAuthor', 'date', 'title', 'analyticTitle', 'monogrTitle', 'editors', 'pubPlace'])
+      : this.swapFields(['author', 'analyticAuthor', 'pubPlace', 'title', 'analyticTitle', 'pubPlace']);
+  }
+
+  private setSelectedItem(selectedField: string): string {
+    let field: string;
+    try {
+      field = Object.values(selectedField)[1];
+    } catch (e) {
+      field = 'Author';
+    }
+
+    return field;
+  }
+
+  sortBy(mode: string, selectedField: string): BibliographicCitation[] {
     const res = this.biblCits.sort((x, y) => {
       if (this.isComplete) {
-        if (field === 'Title') {
-          const t = 'title';
-          try {
-            return x[t].localeCompare(y[t], undefined, { sensitivity: 'base' });
-          } catch (e) {
-            const at = 'analytic_titles';
-
-            return x[at].localeCompare(y[at], undefined, { sensitivity: 'base' });
-          }
-        } else if (field === 'Date') {
-          const d = 'dates';
-          try {
-            return x[d].localeCompare(y[d], undefined, { sensitivity: 'base' });
-          } catch (e) {
-            const mid = 'monogr_imprint_dates';
-
-            return x[mid].localeCompare(y[mid], undefined, { sensitivity: 'base' });
-          }
-        } else {
-          const a = 'authors';
-          try {
-            return x[a].localeCompare(y[a], undefined, { sensitivity: 'base' });
-          } catch (e) {
-            const an = 'analytic_authors';
-
-            return x[an].localeCompare(y[an], undefined, { sensitivity: 'base' });
-          }
-        }
-      } else {
+        return x[
+          this.sortingFields[
+          this.setSelectedItem(selectedField)
+          ]
+        ].localeCompare(y[this.sortingFields[this.setSelectedItem(selectedField)]], undefined, { sensitivity: 'base' });
+      }
+      if (!this.isComplete) {
         return String(x).localeCompare(String(y), undefined, { sensitivity: 'base' });
       }
     });
 
-    return mode === 'Ascendent'
+    return this.setSelectedItem(mode) === 'Ascendent' || this.setSelectedItem(mode) === 'Ascendente'
       ? res
       : res.reverse();
   }
 
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    this.subscriptions.forEach(s => s.unsubscribe());
   }
 }
