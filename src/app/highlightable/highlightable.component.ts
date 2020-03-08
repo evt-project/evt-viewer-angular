c11645d59cfc9b2b10e4a0e27899f9c836aed26f
ChiaraDipi <dipi.chiara@gmail.com>
2020-03-08 12:24:36 +0100

    Set EditionLevelSusceptibleComponent as basic component for HighlightableComponent

--- src/app/highlightable/highlightable.component.ts
+++ src/app/highlightable/highlightable.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { EditionLevelSusceptibleComponent } from '../components/edition-level-susceptible/edition-level-susceptible.component';
 import { EntitiesSelectItem } from '../components/entities-select/entities-select.component';
 
 @Component({
   selector: 'evt-highlightable',
   template: '',
 })
-export class HighlightableComponent {
+export class HighlightableComponent extends EditionLevelSusceptibleComponent {
   @Input() highlightData: { highlight: boolean, highlightColor: string };
   @Input() itemsToHighlight: EntitiesSelectItem[];
 }
