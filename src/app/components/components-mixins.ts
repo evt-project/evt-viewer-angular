5fe875ee097560ee48db8c8bb06db7c42b74b796
ChiaraDipi <dipi.chiara@gmail.com>
2020-05-01 11:53:27 +0200

    Add EditionlevelSusceptible mixin
    This mixin should be used for components which have different layouts depending on edition level

--- src/app/components/components-mixins.ts
+++ src/app/components/components-mixins.ts
@@ -1,4 +1,5 @@
 import { Input } from '@angular/core';
+import { EditionLevelType } from '../app.config';
 import { EntitiesSelectItem } from './entities-select/entities-select.component';
 
 // tslint:disable-next-line: no-any
@@ -10,6 +11,14 @@ export function Highlightable<T extends Constructor<{}>>(base: T) {
   class Temporary extends base {
     @Input() highlightData: { highlight: boolean, highlightColor: string };
     @Input() itemsToHighlight: EntitiesSelectItem[];
+  }
+
+  return Temporary;
+}
+
+export function EditionlevelSusceptible<T extends Constructor<{}>>(base: T) {
+  class Temporary extends base {
+    @Input() editionLevel: EditionLevelType;
   }
 
   return Temporary;
