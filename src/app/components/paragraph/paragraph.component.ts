f63975d575ed008efa3ef26aa067e958ba48d1d0
ChiaraDipi <dipi.chiara@gmail.com>
2020-05-01 12:00:51 +0200

    Add EditionlevelSusceptible extension in Paragraph

--- src/app/components/paragraph/paragraph.component.ts
+++ src/app/components/paragraph/paragraph.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 import { Paragraph } from '../../models/evt-models';
 import { register } from '../../services/component-register.service';
-import { Base, Highlightable } from '../components-mixins';
+import { Base, EditionlevelSusceptible, Highlightable } from '../components-mixins';
 
 @Component({
   selector: 'evt-paragraph',
@@ -11,6 +11,6 @@ import { Base, Highlightable } from '../components-mixins';
 })
 
 @register(Paragraph)
-export class ParagraphComponent extends Highlightable(Base) {
+export class ParagraphComponent extends EditionlevelSusceptible(Highlightable(Base)) {
   @Input() data: Paragraph;
 }
