7c404b26a4ba5de0d3fb3e563d71af8c2457ad91
ChiaraDipi <dipi.chiara@gmail.com>
2020-05-01 12:00:25 +0200

    Add EditionlevelSusceptible extension in GenericElement

--- src/app/components/generic-element/generic-element.component.ts
+++ src/app/components/generic-element/generic-element.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { GenericElement } from '../../models/evt-models';
 import { register } from '../../services/component-register.service';
-import { Base, Highlightable } from '../components-mixins';
+import { Base, EditionlevelSusceptible, Highlightable } from '../components-mixins';
 
 @Component({
   selector: 'evt-generic-element',
@@ -9,6 +9,6 @@ import { Base, Highlightable } from '../components-mixins';
   styleUrls: ['./generic-element.component.scss'],
 })
 @register(GenericElement)
-export class GenericElementComponent extends Highlightable(Base) {
+export class GenericElementComponent extends EditionlevelSusceptible(Highlightable(Base)) {
   @Input() data: GenericElement;
 }
