6926df1194093e37c37d00ca6d8218a34a48c175
ChiaraDipi <dipi.chiara@gmail.com>
2020-06-23 12:05:53 +0200

    Add choiceComponent

---
+++ src/app/components/choice/choice.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input } from '@angular/core';
+
+import { Choice } from '../../models/evt-models';
+import { register } from '../../services/component-register.service';
+import { EditionlevelSusceptible, Highlightable } from '../components-mixins';
+
+export interface ChoiceComponent extends EditionlevelSusceptible, Highlightable { }
+
+@Component({
+  selector: 'evt-choice',
+  templateUrl: './choice.component.html',
+  styleUrls: ['./choice.component.scss'],
+})
+@register(Choice)
+export class ChoiceComponent {
+  @Input() data: Choice;
+
+  get content() {
+    if (this.editionLevel === 'diplomatic') {
+      return this.data.originalContent;
+    }
+
+    return this.data.normalizedContent;
+  }
+
+  get alternativeContent() {
+    if (this.editionLevel === 'diplomatic') {
+      return this.data.normalizedContent;
+    }
+
+    return this.data.originalContent;
+  }
+}
