c31665351b36b6c81ec31c942cbf791109339d6f
Giulia Cacioli <giulia.cacioli@gmail.com>
2020-10-11 13:04:23 +0200

    Refactor parsePages

--- src/app/services/xml-parsers/structure-xml-parser.service.ts
+++ src/app/services/xml-parsers/structure-xml-parser.service.ts
@@ -21,63 +21,78 @@ export class StructureXmlParserService {
 
     if (document) {
       const pageElements = document.querySelectorAll(pageTagName);
-      const l = pageElements.length;
-      if (l > 0) {
-        for (let i = 0; i < l; i++) {
-          const element = pageElements[i];
-          let pageContent: XMLElement[] = [];
-          if (i < l - 1) { // TODO: handle last page
-            if (i === 0) {
-              pageContent = getElementsBetweenTreeNode(element.closest('body'), pageElements[i + 1]);
-            } else {
-              pageContent = getElementsBetweenTreeNode(element, pageElements[i + 1]);
-            }
-          } else {
-            pageContent = getElementsAfterTreeNode(element);
-          }
-          // Exclude nodes in <front>
-          pageContent = pageContent.filter(el => {
-            if (el.nodeType === 3) {
-              return !el.parentElement.closest('front') && el.parentElement.tagName !== 'front';
-            }
-            if (el.nodeType === 1) {
-              return !el.closest('front') && el.tagName !== 'front';
-            }
+      const pagesNumber = pageElements.length;
 
-            return false;
-          });
-
-          const page: Page = {
-            id: element.getAttribute('xml:id') || 'page_' + (pagesIndexes.length + 1),
-            label: element.getAttribute('n') || 'Page ' + (pagesIndexes.length + 1),
-            originalContent: pageContent,
-            parsedContent: pageContent.map(child => this.genericParserService.parse(child as XMLElement)),
-          };
-          pages[page.id] = page;
-          pagesIndexes.push(page.id);
-        }
+      if (pagesNumber > 0) {
+        this.parseDocumentPages(pageElements, pagesNumber, pages, pagesIndexes);
       } else {
-        // No <pb> used => TODO: Decide how to handle text division
-        console.warn('TODO: Decide how to handle text division when there are no <pb>s');
-        const mainText = document.querySelector('text');
-        const content = Array.from(mainText.childNodes);
-        const page: Page = {
-          id: `page_${new Date().getTime()}`,
-          label: 'Main Text',
-          originalContent: content as XMLElement[],
-          parsedContent: content.map(child => this.genericParserService.parse(child as XMLElement))
-            // tslint:disable-next-line: no-string-literal
-            .filter(c => !!c['content']), // TODO: FIXME: fix property access
-        };
-        pages[page.id] = page;
-        pagesIndexes.push(page.id);
+        this.parseDocument(document, pages, pagesIndexes);
       }
-      console.log(pages);
+
+      console.log('### PAGES ###', pages);
     }
 
     return {
       pages,
       pagesIndexes,
     };
+  }
+
+  parseDocumentPages(pageElements: NodeListOf<Element>, pagesNumber: number, pages: Map<Page>, pagesIndexes: string[]) {
+    pageElements.forEach((element, i) => {
+      let pageContent: XMLElement[] = [];
+      const isFirstPage = i === 0;
+      const isLastPage = i === pagesNumber - 1;
+
+      if (isFirstPage) {
+        pageContent = getElementsBetweenTreeNode(element.closest('body'), pageElements[i + 1]);
+      }
+
+      if (isLastPage) {
+        pageContent = getElementsAfterTreeNode(element);
+      }
+
+      if (!isFirstPage && !isLastPage) {
+        pageContent = getElementsBetweenTreeNode(element, pageElements[i + 1]);
+      }
+
+      // Exclude nodes in <front>
+      pageContent = pageContent.filter(el => {
+        if (el.nodeType === 3) {
+          return !el.parentElement.closest('front') && el.parentElement.tagName !== 'front';
+        }
+        if (el.nodeType === 1) {
+          return !el.closest('front') && el.tagName !== 'front';
+        }
+
+        return false;
+      });
+
+      const page: Page = {
+        id: element.getAttribute('xml:id') || 'page_' + (pagesIndexes.length + 1),
+        label: element.getAttribute('n') || 'Page ' + (pagesIndexes.length + 1),
+        originalContent: pageContent,
+        parsedContent: pageContent.map(child => this.genericParserService.parse(child as XMLElement)),
+      };
+      pages[page.id] = page;
+      pagesIndexes.push(page.id);
+    });
+  }
+
+  parseDocument(document: XMLElement, pages: Map<Page>, pagesIndexes: string[]) {
+    // No <pb> used => TODO: Decide how to handle text division
+    console.warn('TODO: Decide how to handle text division when there are no <pb>s');
+    const mainText = document.querySelector('text');
+    const content = Array.from(mainText.childNodes);
+    const page: Page = {
+      id: `page_${new Date().getTime()}`,
+      label: 'Main Text',
+      originalContent: content as XMLElement[],
+      parsedContent: content.map(child => this.genericParserService.parse(child as XMLElement))
+        // tslint:disable-next-line: no-string-literal
+        .filter(c => !!c['content']), // TODO: FIXME: fix property access
+    };
+    pages[page.id] = page;
+    pagesIndexes.push(page.id);
   }
 }
