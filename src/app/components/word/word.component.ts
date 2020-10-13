bb7557ec243b80e96f5a4f5d924558d7e0783529
ChiaraDipi <dipi.chiara@gmail.com>
2020-10-13 18:13:07 +0200

    Add normalization when word broken between two lines

--- src/app/components/word/word.component.ts
+++ src/app/components/word/word.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 
-import { Word } from '../../models/evt-models';
+import { GenericElement, Lb, Word } from '../../models/evt-models';
 import { register } from '../../services/component-register.service';
 import { EditionlevelSusceptible, Highlightable } from '../components-mixins';
 
@@ -14,4 +14,21 @@ export interface WordComponent extends EditionlevelSusceptible, Highlightable { 
 @register(Word)
 export class WordComponent {
   @Input() data: Word;
+
+  get word() {
+    if (this.editionLevel === 'diplomatic') {
+      return this.data.content;
+    }
+
+    const lbIndex = this.data.content.findIndex((el: GenericElement) => el.type === Lb);
+    if (lbIndex >= 0) {
+      const wordContent = [...this.data.content];
+      wordContent.splice(lbIndex, 1);
+      wordContent.push(this.data.content[lbIndex]);
+
+      return wordContent;
+    }
+
+    return this.data.content;
+  }
 }
