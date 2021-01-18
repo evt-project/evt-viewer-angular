ae2afbdc7d7b9ccf3ad6c5911960e8e8cd93418f
ChiaraDipi <dipi.chiara@gmail.com>
2021-01-18 18:42:05 +0100

    Add xi:include support

--- src/app/services/edition-data.service.ts
+++ src/app/services/edition-data.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { catchError, map, publishReplay, refCount } from 'rxjs/operators';
+import { forkJoin, Observable, of } from 'rxjs';
+import { catchError, map, mergeMap, publishReplay, refCount, tap } from 'rxjs/operators';
 import { AppConfig } from '../app.config';
 import { OriginalEncodingNodeType, XMLElement } from '../models/evt-models';
 import { parseXml } from '../utils/xml-utils';
@@ -19,7 +19,9 @@ export class EditionDataService {
   }
 
   private loadAndParseEditionData() {
-    return this.http.get(this.editionUrls[0], { responseType: 'text' }).pipe(
+    const editionUrl = this.editionUrls[0];
+
+    return this.http.get(editionUrl, { responseType: 'text' }).pipe(
       map(source => {
         let editionDoc: XMLElement;
         if (typeof source !== 'object' && typeof source === 'string') {
@@ -30,9 +32,47 @@ export class EditionDataService {
 
         return editionDoc;
       }),
+      mergeMap((editionData) => this.loadXIinclude(editionData, editionUrl.substring(0, editionUrl.lastIndexOf('/') + 1))),
       publishReplay(1),
       refCount(),
       catchError(() => this.handleLoadingError()));
+  }
+
+  loadXIinclude(doc: HTMLElement, baseUrlPath: string) {
+    const filesToInclude = Array.from(doc.getElementsByTagName('xi:include'));
+    const xiIncludeLoadsSubs = filesToInclude.map(element =>
+      this.http.get(baseUrlPath + element.getAttribute('href'), { responseType: 'text' })
+        .pipe(
+          tap((fileData) => {
+            const includedDoc = parseXml(fileData);
+            const fileXpointer = element.getAttribute('xpointer');
+            let includedTextElem: Node;
+            if (fileXpointer) {
+              includedTextElem = doc.querySelector(`[*|id="${fileXpointer}"]`) || includedDoc.querySelector('text');
+            } else {
+              includedTextElem = includedDoc.querySelector('text');
+            }
+            // element.parentNode.replaceChild(includedTextElem, element);
+            element.parentNode.appendChild(includedTextElem);
+          }),
+          catchError(_ => {
+            Array.from(element.getElementsByTagName('xi:fallback')).map((el) => {
+              const divEl = document.createElement('div');
+              divEl.classList.add('xiinclude-fallback');
+              divEl.setAttribute('xml:id', element.getAttribute('xpointer'));
+              divEl.innerHTML = `<p>${el.innerHTML}</p>`;
+
+              return divEl;
+            }).forEach((el) => element.parentNode.replaceChild(el, element));
+
+            return of(doc);
+          }),
+        ));
+    if (xiIncludeLoadsSubs.length > 0) {
+      return forkJoin(xiIncludeLoadsSubs).pipe(map(() => doc));
+    }
+
+    return of(doc);
   }
 
   private handleLoadingError() {
