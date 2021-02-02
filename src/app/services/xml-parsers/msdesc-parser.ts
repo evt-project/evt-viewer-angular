cc7c72552e71b018de494ceb940f4c1b7e2c9809
SaraMaenza <s.maenza1@studenti.unipi.it>
2021-02-02 22:43:31 +0100

    Update provenance in history parser

--- src/app/services/xml-parsers/msdesc-parser.ts
+++ src/app/services/xml-parsers/msdesc-parser.ts
@@ -221,7 +221,7 @@ export class HistoryParser extends GenericElemParser implements Parser<XMLElemen
             type: History,
             acquisition: queryAndParseElement(xml, 'acquisition'),
             origin: queryAndParseElement(xml, 'origin'),
-            provenance: queryAndParseElement(xml, 'provenance'),
+            provenance: queryAndParseElements(xml, 'provenance'),
             summary: queryAndParseElement(xml, 'summary'),
             pEl: queryAndParseElements<Paragraph>(xml, 'p'),
         };
