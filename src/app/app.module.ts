a55477443e68508382478695f421dfc345802e3a
SaraMaenza <s.maenza1@studenti.unipi.it>
2021-02-04 12:31:45 +0100

    Add ms-contents-component in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,6 +37,7 @@ import { GlobalListsComponent } from './components/global-lists/global-lists.com
 import { IdentifierComponent } from './components/identifier/identifier.component';
 import { LbComponent } from './components/lb/lb.component';
 import { ManuscriptThumbnailsViewerComponent } from './components/manuscript-thumbnails-viewer/manuscript-thumbnails-viewer.component';
+import { MsContentsComponent } from './components/ms-contents/ms-contents.component';
 import { MsIdentifierComponent } from './components/ms-identifier/ms-identifier.component';
 import { NamedEntitiesListComponent } from './components/named-entities-list/named-entities-list.component';
 import { NamedEntityRefComponent } from './components/named-entity-ref/named-entity-ref.component';
@@ -127,6 +128,7 @@ export function initializeApp(appConfig: AppConfig) {
     MainMenuComponent,
     ManuscriptThumbnailsViewerComponent,
     MsDescSectionComponent,
+    MsContentsComponent,
     MsIdentifierComponent,
     NamedEntitiesListComponent,
     NamedEntityComponent,
