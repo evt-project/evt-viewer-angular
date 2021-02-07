620ac4ce3a0c5666b780c25812a2bee8cf619ccf
SaraMaenza <s.maenza1@studenti.unipi.it>
2021-02-07 12:44:41 +0100

    Fix decoNote and binding elements in BindingDesc and Binding parsers

--- src/app/services/xml-parsers/msdesc-parser.ts
+++ src/app/services/xml-parsers/msdesc-parser.ts
@@ -351,7 +351,7 @@ export class BindingParser extends GenericElemParser implements Parser<XMLElemen
             ...super.parse(xml),
             type: Binding,
             contemporary: true || false,
-            decoNote: queryAndParseElement(xml, 'decoNote', createParser(DecoNoteParser, this.genericParse)),
+            decoNote: queryAndParseElements(xml, 'decoNote', createParser(DecoNoteParser, this.genericParse)),
             pEl: queryAndParseElements(xml, 'p', createParser(ParagraphParser, this.genericParse)),
             condition: unhandledElement(xml, 'condition', this.genericParse),
             ab: unhandledElement(xml, 'ab', this.genericParse),
@@ -366,8 +366,9 @@ export class BindingDescParser extends GenericElemParser implements Parser<XMLEl
             ...super.parse(xml),
             type: BindingDesc,
             condition: unhandledElement(xml, 'condition', this.genericParse),
-            decoNote: queryAndParseElement(xml, 'decoNote', createParser(DecoNoteParser, this.genericParse)),
-            binding: queryAndParseElement(xml, 'binding', createParser(BindingParser, this.genericParse)),
+            decoNote: queryAndParseElements(xml, 'decoNote', createParser(DecoNoteParser, this.genericParse)),
+            binding: queryAndParseElements(xml, 'binding', createParser(BindingParser, this.genericParse)),
+            pEl: queryAndParseElements<Paragraph>(xml, 'p', createParser(ParagraphParser, this.genericParse)),
         };
     }
 }
