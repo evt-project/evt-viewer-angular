e667b1cd014ac79d433a1ae8d873298c16ceccc7
SaraMaenza <s.maenza1@studenti.unipi.it>
2021-02-08 10:51:08 +0100

    Fix msFrag component

--- src/app/components/ms-frag/ms-frag.component.ts
+++ src/app/components/ms-frag/ms-frag.component.ts
@@ -5,7 +5,7 @@ import { register } from '../../services/component-register.service';
 @Component({
   selector: 'evt-ms-frag',
   templateUrl: './ms-frag.component.html',
-  styleUrls: ['./ms-frag.component.scss']
+  styleUrls: ['./ms-frag.component.scss'],
 })
 
 @register(MsFrag)
