95ca3f5b04a88009efad3540eff6f5ca554493fe
ChiaraDipi <dipi.chiara@gmail.com>
2021-02-18 16:42:49 +0100

    Add TitleStmt component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -59,6 +59,7 @@ import { SicComponent } from './components/sic/sic.component';
 import { SuppliedComponent } from './components/supplied/supplied.component';
 import { SurplusComponent } from './components/surplus/surplus.component';
 import { TextComponent } from './components/text/text.component';
+import { TitleStmtComponent } from './components/title-stmt/title-stmt.component';
 import { VerseComponent } from './components/verse/verse.component';
 import { VersesGroupComponent } from './components/verses-group/verses-group.component';
 import { WordComponent } from './components/word/word.component';
@@ -167,6 +168,7 @@ export function initializeApp(appConfig: AppConfig) {
     TextSourcesComponent,
     TextTextComponent,
     TextVersionsComponent,
+    TitleStmtComponent,
     VerseComponent,
     VersesGroupComponent,
     VersionPanelComponent,
