e343ec623b4cfb0a85855c211aed868b9207b9b7
SaraMaenza <s.maenza1@studenti.unipi.it>
2021-03-02 17:26:20 +0100

    Add input listClass in Identifier.component

--- src/app/components/identifier/identifier.component.ts
+++ src/app/components/identifier/identifier.component.ts
@@ -5,10 +5,11 @@ import { register } from '../../services/component-register.service';
 @Component({
   selector: 'evt-identifier',
   templateUrl: './identifier.component.html',
-  styleUrls: ['./identifier.component.scss']
+  styleUrls: ['./identifier.component.scss'],
 })
 
 @register(Identifier)
 export class IdentifierComponent {
   @Input() data: Identifier;
+  @Input() listClass: boolean;
 }
