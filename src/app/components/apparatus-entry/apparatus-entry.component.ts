a9a1b03f20f8e535541332db90e316a9fbfa8762
Giulia Cacioli <giulia.cacioli@gruppometa.it>
2021-04-11 12:59:55 +0200

    Handle nested apparatus entries

--- src/app/components/apparatus-entry/apparatus-entry.component.ts
+++ src/app/components/apparatus-entry/apparatus-entry.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Optional, SkipSelf } from '@angular/core';
 import { map, shareReplay } from 'rxjs/operators';
 import { ApparatusEntry } from '../../models/evt-models';
 import { register } from '../../services/component-register.service';
 import { EVTModelService } from '../../services/evt-model.service';
 import { EditionlevelSusceptible } from '../components-mixins';
+import { ApparatusEntryDetailComponent } from './apparatus-entry-detail/apparatus-entry-detail.component';
 
 export interface ApparatusEntryComponent extends EditionlevelSusceptible { }
 @Component({
@@ -16,6 +17,7 @@ export class ApparatusEntryComponent {
   @Input() data: ApparatusEntry;
 
   public opened = false;
+  public isNestedInApp: boolean;
 
   variance$ = this.evtModelService.appVariance$.pipe(
     map((variances) => variances[this.data.id]),
@@ -24,10 +26,13 @@ export class ApparatusEntryComponent {
 
   constructor(
     private evtModelService: EVTModelService,
+    @Optional() @SkipSelf() private parentDetailComponent?: ApparatusEntryDetailComponent,
   ) {
+    this.isNestedInApp = this.parentDetailComponent ? true : false;
   }
 
-  toggleAppEntryBox() {
+  toggleAppEntryBox(e: MouseEvent) {
+    e.stopPropagation();
     this.opened = !this.opened;
   }
 
