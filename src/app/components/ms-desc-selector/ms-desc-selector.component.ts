9bae0f1c8dbd2d268129836bc4aa3da6c4b813c1
SaraMaenza <s.maenza1@studenti.unipi.it>
2021-06-15 18:20:07 +0200

    Add ms-desc-selector component

---
+++ src/app/components/ms-desc-selector/ms-desc-selector.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'evt-ms-desc-selector',
+  templateUrl: './ms-desc-selector.component.html',
+  styleUrls: ['./ms-desc-selector.component.scss'],
+})
+export class MsDescSelectorComponent {
+
+}
