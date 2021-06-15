cedb959f939931decf1e89a82da68714a9b7f2cc
SaraMaenza <s.maenza1@studenti.unipi.it>
2021-06-15 18:23:45 +0200

    Update msDesc-selector component

--- src/app/components/ms-desc-selector/ms-desc-selector.component.ts
+++ src/app/components/ms-desc-selector/ms-desc-selector.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
+import { BehaviorSubject, combineLatest } from 'rxjs';
+import { distinctUntilChanged, filter, map } from 'rxjs/operators';
+import { EVTModelService } from '../../services/evt-model.service';
 
 @Component({
   selector: 'evt-ms-desc-selector',
@@ -6,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./ms-desc-selector.component.scss'],
 })
 export class MsDescSelectorComponent {
+  public msDesc$ = this.evtModelService.msDesc$;
+
+  // tslint:disable-next-line: variable-name
+  private _msDescID: string;
+  @Input() set msDescID(ms: string) {
+    this._msDescID = ms;
+    this.selectedMsDesc$.next(this._msDescID);
+  }
+  get msDescID() { return this._msDescID; }
+
+  selectedMsDesc$ = new BehaviorSubject<string>(undefined);
+
+  @Output() selectionChange = combineLatest([
+    this.msDesc$,
+    this.selectedMsDesc$.pipe(distinctUntilChanged()),
+  ]).pipe(
+    filter(([msDesc, msDescID]) => !!msDescID && !!msDesc && msDesc.length > 0),
+    map(([msDesc, msDescID]) => msDesc.find(ms => ms.id === msDescID)),
+  );
+
+  constructor(
+    private evtModelService: EVTModelService,
+  ) {
+  }
 
 }
