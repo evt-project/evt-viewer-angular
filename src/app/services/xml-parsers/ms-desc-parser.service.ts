4e78e593a43d61155b01ea23322ad8976289054c
SaraMaenza <s.maenza1@studenti.unipi.it>
2021-06-18 11:00:49 +0200

    Update ms-desc-parser-service

--- src/app/services/xml-parsers/ms-desc-parser.service.ts
+++ src/app/services/xml-parsers/ms-desc-parser.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { parse } from '.';
+import { MsDesc, XMLElement } from '../../models/evt-models';
+import { MsDescParser } from './msdesc-parser';
+import { createParser } from './parser-models';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MsDescParserService {
 
+  parseMsDesc(xml: XMLElement): MsDesc[] {
+    if (!xml) { return []; }
+
+    return Array.from(xml.querySelectorAll<XMLElement>('msDesc')).map(ms => {
+      const msDesc = ms.querySelector<HTMLElement>('msDesc');
+      const msDescParser = createParser(MsDescParser, parse);
+
+      return msDescParser.parse(msDesc);
+    });
 }}
