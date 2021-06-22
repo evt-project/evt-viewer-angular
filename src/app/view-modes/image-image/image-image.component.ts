32cf647efb7da77b7060d58cca92a4fbd4bc23e3
SaraMaenza <s.maenza1@studenti.unipi.it>
2021-06-22 14:35:11 +0200

    Add image-image component

---
+++ src/app/view-modes/image-image/image-image.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'evt-image-image',
+  templateUrl: './image-image.component.html',
+  styleUrls: ['./image-image.component.scss']
+})
+export class ImageImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
