0ad94544bbdfa4bdaf2e4fb80eaa75f007f251e5
SaraMaenza <s.maenza1@studenti.unipi.it>
2021-06-23 18:46:54 +0200

    Update image-panel component

--- src/app/panels/image-panel/image-panel.component.ts
+++ src/app/panels/image-panel/image-panel.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { EVTModelService } from 'src/app/services/evt-model.service';
 import { AppConfig } from '../../app.config';
 
 @Component({
@@ -10,4 +11,33 @@ export class ImagePanelComponent {
   manifest = AppConfig.evtSettings.files.manifestURL !== '' && !!AppConfig.evtSettings.files.manifestURL
     ? AppConfig.evtSettings.files.manifestURL
     : undefined;
+
+  public msDesc$ = this.evtModelService.msDesc$;
+
+  // tslint:disable-next-line: variable-name
+  private _msDescID: string;
+  @Input() set msDescID(p: string) {
+     this._msDescID = p;
+   }
+
+  get msDescID() { return this._msDescID; }
+
+  private showSecondaryContent = false;
+
+  constructor(
+    public evtModelService: EVTModelService,
+  ) {
+  }
+
+  isSecondaryContentOpened(): boolean {
+    return this.showSecondaryContent;
+  }
+
+  toggleMsDescContent(){
+    this.showSecondaryContent = true;
+  }
+
+  resetMsDesc(){
+    this.showSecondaryContent = false;
+  }
 }
