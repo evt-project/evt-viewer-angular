1f226656d4b359c629107c8223a35aa10ad76eab
SaraMaenza <s.maenza1@studenti.unipi.it>
2021-06-23 18:46:54 +0200

    Update image-panel component

--- src/app/panels/image-panel/image-panel.component.ts
+++ src/app/panels/image-panel/image-panel.component.ts
@@ -1,6 +1,5 @@
-import { Component, Input } from '@angular/core';
-import { BehaviorSubject, combineLatest } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { ViewerDataType } from '../../models/evt-models';
 import { EVTModelService } from '../../services/evt-model.service';
 
@@ -9,27 +8,60 @@ import { EVTModelService } from '../../services/evt-model.service';
   templateUrl: './image-panel.component.html',
   styleUrls: ['./image-panel.component.scss'],
 })
-export class ImagePanelComponent {
+export class ImagePanelComponent implements OnInit{
   @Input() viewerData: ViewerDataType;
 
-  currentMsDescId$ = new BehaviorSubject(undefined);
-  currentMsDesc$ = combineLatest([this.evtModelService.msDesc$, this.currentMsDescId$]).pipe(
-    filter(([msDesc, currentId]) => !!msDesc && !!currentId),
-    map(([msDesc, currentId]) => msDesc.find(m => m.id === currentId)),
-  );
-
-  msDescOpen = false;
+  get imageIndex() { return this._imageIndex; }
 
   constructor(
-    private evtModelService: EVTModelService,
+    public evtModelService: EVTModelService,
+    public http: HttpClient,
+    private cdref: ChangeDetectorRef,
   ) {
   }
+  public msDesc$ = this.evtModelService.msDesc$;
+  public showSecondaryContent = false;
+  public selectedPage;
+  public msDescID = '';
+  public value: number;
+  public images = [];
+  public currentImg: string;
+  // tslint:disable-next-line: variable-name
+  private _imageIndex: number;
+  public manifest;
 
-  setMsDescOpen(isOpen: boolean) {
-    this.msDescOpen = isOpen;
+  toggleImg(event){
+    this._imageIndex = this.images.indexOf(event);
   }
 
-  setMsDescID(msDescId: string) {
-    this.currentMsDescId$.next(msDescId);
+  eventPageImg(pageImg: number){
+    this.currentImg = this.images[pageImg - 1].label;
+    this.cdref.detectChanges();
   }
+
+  ngOnInit() {
+    if (this.viewerData.type === 'manifest'){
+      this.manifest = this.viewerData.value?.manifestURL;
+      // tslint:disable-next-line: no-any
+      this.http.get(`${this.manifest}`).subscribe((item: any) => {
+        item.sequences.forEach((value) => {
+          this.images.push(...value.canvases);
+        });
+        this.currentImg = this.images[0].label;
+      });
+    }
+  }
+
+  isSecondaryContentOpened(): boolean {
+    return this.showSecondaryContent;
+  }
+
+  isMsDescOpen(event: boolean){
+    this.showSecondaryContent = event;
+  }
+
+  setMsDescID(idMsDesc: string){
+    this.msDescID = idMsDesc;
+  }
+
 }
