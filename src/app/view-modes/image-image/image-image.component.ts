950601fd6da4c554931a8091fbf12dd67edcc6e1
SaraMaenza <s.maenza1@studenti.unipi.it>
2021-06-24 10:14:40 +0200

    Update image-image component

--- src/app/view-modes/image-image/image-image.component.ts
+++ src/app/view-modes/image-image/image-image.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { DisplayGrid, GridsterConfig, GridsterItem, GridType } from 'angular-gridster2';
 
 @Component({
   selector: 'evt-image-image',
   templateUrl: './image-image.component.html',
-  styleUrls: ['./image-image.component.scss']
+  styleUrls: ['./image-image.component.scss'],
 })
 export class ImageImageComponent implements OnInit {
+  public layoutOptions: GridsterConfig = {};
+  public imagePanel1Item: GridsterItem = { cols: 1, rows: 1, y: 0, x: 0 };
+  public imagePanel2Item: GridsterItem = { cols: 1, rows: 1, y: 0, x: 1 };
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.initGridster();
   }
 
+  private initGridster() {
+    this.layoutOptions = {
+      gridType: GridType.Fit,
+      displayGrid: DisplayGrid.None,
+      margin: 0,
+      maxCols: 2,
+      maxRows: 1,
+      draggable: {
+        enabled: true,
+        ignoreContent: true,
+        dragHandleClass: 'panel-header',
+      },
+      resizable: {
+        enabled: false,
+      },
+    };
+  }
 }
