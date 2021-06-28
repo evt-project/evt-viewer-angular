11218006ebac638dc7ed17f48c92f5069eb291b3
SaraMaenza <s.maenza1@studenti.unipi.it>
2021-06-28 12:15:56 +0200

    Update image-panel component

--- src/app/panels/image-panel/image-panel.component.ts
+++ src/app/panels/image-panel/image-panel.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { EVTModelService } from 'src/app/services/evt-model.service';
 import { AppConfig } from '../../app.config';
 
@@ -7,29 +8,35 @@ import { AppConfig } from '../../app.config';
   templateUrl: './image-panel.component.html',
   styleUrls: ['./image-panel.component.scss'],
 })
-export class ImagePanelComponent {
+export class ImagePanelComponent implements OnInit  {
   manifest = AppConfig.evtSettings.files.manifestURL !== '' && !!AppConfig.evtSettings.files.manifestURL
     ? AppConfig.evtSettings.files.manifestURL
     : undefined;
 
-  public msDesc$ = this.evtModelService.msDesc$;
-
-  public childData = [];
-
-  // tslint:disable-next-line: variable-name
-  private _msDescID: string;
   @Input() set msDescID(p: string) {
      this._msDescID = p;
    }
 
   get msDescID() { return this._msDescID; }
 
-  private showSecondaryContent = false;
+  get imageIndex() { return this._imageIndex; }
 
   constructor(
     public evtModelService: EVTModelService,
+    public http: HttpClient,
+    private cdref: ChangeDetectorRef,
   ) {
   }
+  public value: number;
+  public msDesc$ = this.evtModelService.msDesc$;
+  public images = [];
+  public currentImg: string;
+  // tslint:disable-next-line: variable-name
+  private _msDescID: string;
+  // tslint:disable-next-line: variable-name
+  private _imageIndex: number;
+  public page = 0;
+  private showSecondaryContent = false;
 
   isSecondaryContentOpened(): boolean {
     return this.showSecondaryContent;
@@ -43,8 +50,28 @@ export class ImagePanelComponent {
     this.showSecondaryContent = false;
   }
 
-  eventHandler(event: []){
-    this.childData = event;
+  toggleImg(event){
+    this._imageIndex = this.images.indexOf(event);
+  }
+
+  eventHandler(event: number){
+    this.page = event;
+    this.currentImg = this.images[this.page].label;
+    this.cdref.detectChanges();
+  }
+
+  ngOnInit() {
+    // tslint:disable-next-line: no-any
+    this.http.get(`${this.manifest}`).subscribe((item: any) => {
+      item.sequences.forEach((value) => {
+        this.images.push(...value.canvases);
+      });
+      this._imageIndex = 0;
+      this.currentImg = this.images[this.page].label;
+      this.cdref.detectChanges();
+
+      return this.images;
+    });
   }
 
 }
