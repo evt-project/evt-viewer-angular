3246bde7971ee1936d5170851fa49a9786b92bdc
ChiaraDipi <dipi.chiara@gmail.com>
2021-10-27 15:32:05 +0200

    Add navigation buttons in navbar

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { combineLatest } from 'rxjs';
+import { map, take } from 'rxjs/operators';
+import { EVTModelService } from '../services/evt-model.service';
+import { EVTStatusService } from '../services/evt-status.service';
 
 @Component({
   selector: 'evt-nav-bar',
@@ -6,5 +10,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent {
+  private currentPageInfo$ = combineLatest([
+    this.evtModelService.pages$,
+    this.evtStatusService.currentPage$,
+  ]);
 
+  prevNavigationDisabled$ = this.currentPageInfo$.pipe(
+    map(([pages, page]) => pages.findIndex((p) => p.id === page.id) === 0),
+  );
+  nextNavigationDisabled$ = this.currentPageInfo$.pipe(
+    map(([pages, page]) => pages.findIndex((p) => p.id === page.id) === pages.length - 1),
+  );
+
+  constructor(
+    private evtStatusService: EVTStatusService,
+    private evtModelService: EVTModelService,
+  ) {
+  }
+
+  goToFirstPage() {
+    this.evtModelService.pages$.pipe(take(1)).subscribe(
+      (pages) => this.evtStatusService.updatePage$.next(pages[0]),
+    );
+  }
+
+  goToPrevPage() {
+    combineLatest([
+      this.evtModelService.pages$,
+      this.evtStatusService.currentPage$,
+    ]).pipe(take(1)).subscribe(([pages, page]) => {
+      const pageIndex = pages.findIndex((p) => p.id === page.id);
+      if (pageIndex > 0) {
+        this.evtStatusService.updatePage$.next(pages[pageIndex - 1]);
+      }
+    });
+  }
+
+  goToNextPage() {
+    combineLatest([
+      this.evtModelService.pages$,
+      this.evtStatusService.currentPage$,
+    ]).pipe(take(1)).subscribe(([pages, page]) => {
+      const pageIndex = pages.findIndex((p) => p.id === page.id);
+      if (pageIndex < pages.length - 1) {
+        this.evtStatusService.updatePage$.next(pages[pageIndex + 1]);
+      }
+    });
+  }
+
+  goToLastPage() {
+    this.evtModelService.pages$.pipe(take(1)).subscribe(
+      (pages) => this.evtStatusService.updatePage$.next(pages[pages.length - 1]),
+    );
+  }
 }
