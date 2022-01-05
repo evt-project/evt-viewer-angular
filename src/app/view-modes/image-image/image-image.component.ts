d71f0434ba26e163b75c10588365d85cad37e747
SaraMaenza <s.maenza1@studenti.unipi.it>
2022-01-05 19:44:28 +0100

    Update image-image component

--- src/app/view-modes/image-image/image-image.component.ts
+++ src/app/view-modes/image-image/image-image.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { DisplayGrid, GridsterConfig, GridsterItem, GridType } from 'angular-gridster2';
 
 @Component({
@@ -6,30 +6,9 @@ import { DisplayGrid, GridsterConfig, GridsterItem, GridType } from 'angular-gri
   templateUrl: './image-image.component.html',
   styleUrls: ['./image-image.component.scss'],
 })
-export class ImageImageComponent implements OnInit {
-  public layoutOptions: GridsterConfig = {};
+export class ImageImageComponent {
+  // tslint:disable-next-line: max-line-length
+  public layoutOptions: GridsterConfig = { gridType: GridType.Fit, displayGrid: DisplayGrid.None, margin: 0, maxCols: 2, maxRows: 1, draggable: { enabled: true, ignoreContent: true, dragHandleClass: 'panel-header'}, resizable: { enabled: false }};
   public imagePanel1Item: GridsterItem = { cols: 1, rows: 1, y: 0, x: 0 };
   public imagePanel2Item: GridsterItem = { cols: 1, rows: 1, y: 0, x: 1 };
-
-  ngOnInit() {
-    this.initGridster();
-  }
-
-  private initGridster() {
-    this.layoutOptions = {
-      gridType: GridType.Fit,
-      displayGrid: DisplayGrid.None,
-      margin: 0,
-      maxCols: 2,
-      maxRows: 1,
-      draggable: {
-        enabled: true,
-        ignoreContent: true,
-        dragHandleClass: 'panel-header',
-      },
-      resizable: {
-        enabled: false,
-      },
-    };
-  }
 }
