aa7a3618323a0123cc9b0d4ce74c71c0cc42f89f
ChiaraDipi <dipi.chiara@gmail.com>
2022-04-20 11:00:09 +0200

    Switch from tslint to eslint
    Minimum rules retrieved. WIP to retrieve all old rules

--- src/app/ui-components/directives/click-outside.directive.ts
+++ src/app/ui-components/directives/click-outside.directive.ts
@@ -1,7 +1,7 @@
 import { Directive, ElementRef, EventEmitter, HostListener, Output } from '@angular/core';
 
 @Directive({
-  // tslint:disable-next-line: directive-selector
+   // eslint-disable-next-line @angular-eslint/directive-selector
   selector: '[clickOutside]',
 })
 export class ClickOutsideDirective {
