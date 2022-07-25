1d4c5d982f1885dbd2d29a4138c799f874d4c36a
ChiaraDipi <dipi.chiara@gmail.com>
2022-07-25 11:09:07 +0200

    Remove OnInit from TextVersion component

--- src/app/view-modes/text-versions/text-versions.component.ts
+++ src/app/view-modes/text-versions/text-versions.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { CompactType, DisplayGrid, GridsterConfig, GridsterItem, GridType } from 'angular-gridster2';
 import { map, shareReplay } from 'rxjs/operators';
 import { EditionLevel } from 'src/app/app.config';
@@ -11,14 +11,47 @@ import { EvtIconInfo } from 'src/app/ui-components/icon/icon.component';
   templateUrl: './text-versions.component.html',
   styleUrls: ['./text-versions.component.scss'],
 })
-export class TextVersionsComponent implements OnInit {
+export class TextVersionsComponent {
   @ViewChild('versionsPanel', { static: true }) versionsPanel: ElementRef;
   private versions: VersionItem[] = [];
 
-  public options: GridsterConfig = {};
+  public options: GridsterConfig = {
+    gridType: GridType.Fit,
+    displayGrid: DisplayGrid.None,
+    margin: 0,
+    maxCols: 2,
+    maxRows: 1,
+    draggable: {
+      enabled: false,
+      ignoreContent: false,
+      ignoreContentClass: 'panel-content',
+      dragHandleClass: 'panel-header',
+    },
+    resizable: {
+      enabled: false,
+    },
+  };
   public textPanelItem: GridsterItem = { cols: 1, rows: 1, y: 0, x: 0 };
   public versionsPanelItem: GridsterItem = { cols: 1, rows: 1, y: 0, x: 1 };
-  public versionsOptions: GridsterConfig = {};
+  public versionsOptions: GridsterConfig = {
+    gridType: GridType.Fit,
+    displayGrid: DisplayGrid.None,
+    compactType: CompactType.CompactLeft,
+    scrollToNewItems: true,
+    margin: 0,
+    maxRows: 1,
+    draggable: {
+      enabled: true,
+      ignoreContent: true,
+      dragHandleClass: 'panel-header',
+    },
+    resizable: {
+      enabled: false,
+    },
+    mobileBreakpoint: 0,
+    itemResizeCallback: this.updateFixedColWidth.bind(this),
+    itemChangeCallback: this.itemChange.bind(this),
+  };
 
   public currentPageID$ = this.evtStatusService.currentStatus$.pipe(
     map(({ page }) => page.id),
@@ -41,11 +74,6 @@ export class TextVersionsComponent implements OnInit {
   constructor(
     private evtStatusService: EVTStatusService,
   ) {
-  }
-
-  ngOnInit() {
-    this.initGridster();
-    this.initPageAndVersions();
   }
 
   changePage(selectedPage: Page) {
@@ -73,48 +101,6 @@ export class TextVersionsComponent implements OnInit {
   removeVersion(index) {
     this.versions.splice(index, 1);
     this.updateGridsterOptions();
-  }
-
-  private initPageAndVersions() {
-    // TODO: subscribe to route params
-  }
-
-  private initGridster() {
-    this.options = {
-      gridType: GridType.Fit,
-      displayGrid: DisplayGrid.None,
-      margin: 0,
-      maxCols: 2,
-      maxRows: 1,
-      draggable: {
-        enabled: false,
-        ignoreContent: false,
-        ignoreContentClass: 'panel-content',
-        dragHandleClass: 'panel-header',
-      },
-      resizable: {
-        enabled: false,
-      },
-    };
-    this.versionsOptions = {
-      gridType: GridType.Fit,
-      displayGrid: DisplayGrid.None,
-      compactType: CompactType.CompactLeft,
-      scrollToNewItems: true,
-      margin: 0,
-      maxRows: 1,
-      draggable: {
-        enabled: true,
-        ignoreContent: true,
-        dragHandleClass: 'panel-header',
-      },
-      resizable: {
-        enabled: false,
-      },
-      mobileBreakpoint: 0,
-      itemResizeCallback: this.updateFixedColWidth.bind(this),
-      itemChangeCallback: this.itemChange.bind(this),
-    };
   }
 
   private itemChange() {
