8fe7443ded0b63d3aaec067dfe944c3e80841f67
ChiaraDipi <dipi.chiara@gmail.com>
2022-07-25 14:43:11 +0200

    Improve bindings and events in navBar

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,7 +1,6 @@
-import { ChangeContext } from '@angular-slider/ngx-slider';
-import { AfterViewChecked, ChangeDetectorRef, Component, ElementRef, ViewChild } from '@angular/core';
-import { BehaviorSubject, combineLatest } from 'rxjs';
-import { map, take } from 'rxjs/operators';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { combineLatest, Subject } from 'rxjs';
+import { delay, filter, map, scan, startWith, tap, withLatestFrom } from 'rxjs/operators';
 import { AppConfig } from '../app.config';
 import { EVTModelService } from '../services/evt-model.service';
 import { EVTStatusService } from '../services/evt-status.service';
@@ -11,35 +10,70 @@ import { EVTStatusService } from '../services/evt-status.service';
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss'],
 })
-export class NavBarComponent implements AfterViewChecked {
-  @ViewChild('thumbnailsContainer') thumbnailsContainer: ElementRef;
-
-  private currentPageInfo$ = combineLatest([
-    this.evtModelService.pages$,
-    this.evtStatusService.currentPage$,
-  ]);
-
-  prevNavigationDisabled$ = this.currentPageInfo$.pipe(
-    map(([pages, page]) => pages.findIndex((p) => p.id === page.id) === 0),
+export class NavBarComponent {
+  updateThContainerInfo$ = new Subject<HTMLElement>();
+  thContainerInfo$ = this.updateThContainerInfo$.pipe(
+    scan((currentEl, val) => val || currentEl, undefined),
+    filter(thContainer => !!thContainer),
+    map((thContainer: HTMLElement) => ({
+      width: thContainer.clientWidth,
+      height: thContainer.clientHeight,
+      thumbWidth: parseInt(window.getComputedStyle(document.documentElement).getPropertyValue('--thumbnail-width'), 10),
+      thumbHeight: parseInt(window.getComputedStyle(document.documentElement).getPropertyValue('--thumbnail-height'), 10),
+    })),
   );
-  nextNavigationDisabled$ = this.currentPageInfo$.pipe(
-    map(([pages, page]) => pages.findIndex((p) => p.id === page.id) === pages.length - 1),
+  @ViewChild('thumbnailsContainer') set thumbnailsContainer(el: ElementRef) {
+    this.updateThContainerInfo$.next(el?.nativeElement);
+  }
+
+  thViewerSettings$ = this.thContainerInfo$.pipe(
+    delay(0),
+    map(({ width, height, thumbHeight, thumbWidth }) => ({
+      col: Math.floor(width / thumbWidth),
+      row: Math.floor(height / thumbHeight),
+    })),
+    startWith({ col: 1, row: 1 }),
   );
 
-  currentPage$ = this.currentPageInfo$.pipe(
-    map(([pages, page]) => pages.findIndex((p) => p.id === page.id)),
+  currentPageIndexStatic;
+  currentPageIndex$ = this.evtStatusService.currentPage$.pipe(
+    withLatestFrom(this.evtModelService.pages$),
+    filter(p => !!p),
+    map(([page, pages]) => pages.findIndex((p) => p.id === page.id)),
+    tap(i => this.currentPageIndexStatic = i),
   );
-
-  thViewerSettings$ = new BehaviorSubject({ col: 1, row: 1 });
 
   thumbnailsButton = AppConfig.evtSettings.ui.thumbnailsButton;
-  thumbnailsPanelOpened$ = new BehaviorSubject(false);
+  toggleThumbnailsPanel$ = new Subject<boolean>();
+  thumbnailsPanelOpened$ = this.toggleThumbnailsPanel$.pipe(
+    scan((currentState: boolean, val: boolean | undefined) => val === undefined ? !currentState : val, false),
+    startWith(false),
+  );
 
   viscollButton = AppConfig.evtSettings.ui.viscollButton;
-  viscollPanelOpened$ = new BehaviorSubject(false);
+  toggleViscollPanel$ = new Subject<boolean>();
+  viscollPanelOpened$ = this.toggleViscollPanel$.pipe(
+    scan((currentState: boolean, val: boolean | undefined) => val === undefined ? !currentState : val, false),
+    startWith(false),
+  );
 
   navigationDisabled$ = combineLatest([this.thumbnailsPanelOpened$, this.viscollPanelOpened$]).pipe(
     map(([thumbnailsPanelOpened, viscollPanelOpened]) => thumbnailsPanelOpened || viscollPanelOpened),
+  );
+
+  prevNavigationDisabled$ = combineLatest([
+    this.navigationDisabled$,
+    this.currentPageIndex$,
+  ]).pipe(
+    map(([navDisabled, currentIndex]) => navDisabled || currentIndex === 0),
+  );
+
+  nextNavigationDisabled$ = combineLatest([
+    this.navigationDisabled$,
+    this.currentPageIndex$,
+  ]).pipe(
+    withLatestFrom(this.evtModelService.pages$),
+    map(([[navDisabled, currentIndex], pages]) => navDisabled || currentIndex === pages.length - 1),
   );
 
   pageSliderOptions$ = combineLatest([this.navigationDisabled$, this.evtModelService.pages$])
@@ -53,91 +87,23 @@ export class NavBarComponent implements AfterViewChecked {
       })),
     );
 
+  @HostListener('window:resize') resize() {
+    this.updateThContainerInfo$.next();
+  }
+
   constructor(
-    private evtStatusService: EVTStatusService,
-    private cdref: ChangeDetectorRef,
+    public evtStatusService: EVTStatusService,
     public evtModelService: EVTModelService,
   ) {
   }
 
-  ngAfterViewChecked() {
-    this.calculateThumbsPerPage();
-  }
-
-  changePage(event: ChangeContext) {
-    this.evtModelService.pages$.pipe(take(1)).subscribe(
-      (pages) => this.evtStatusService.updatePage$.next(pages[event.value]),
-    );
-  }
-
-  goToFirstPage() {
-    this.evtModelService.pages$.pipe(take(1)).subscribe(
-      (pages) => this.evtStatusService.updatePage$.next(pages[0]),
-    );
-  }
-
-  goToPrevPage() {
-    combineLatest([
-      this.evtModelService.pages$,
-      this.evtStatusService.currentPage$,
-    ]).pipe(take(1)).subscribe(([pages, page]) => {
-      const pageIndex = pages.findIndex((p) => p.id === page.id);
-      if (pageIndex > 0) {
-        this.evtStatusService.updatePage$.next(pages[pageIndex - 1]);
-      }
-    });
-  }
-
-  goToNextPage() {
-    combineLatest([
-      this.evtModelService.pages$,
-      this.evtStatusService.currentPage$,
-    ]).pipe(take(1)).subscribe(([pages, page]) => {
-      const pageIndex = pages.findIndex((p) => p.id === page.id);
-      if (pageIndex < pages.length - 1) {
-        this.evtStatusService.updatePage$.next(pages[pageIndex + 1]);
-      }
-    });
-  }
-
-  goToLastPage() {
-    this.evtModelService.pages$.pipe(take(1)).subscribe(
-      (pages) => this.evtStatusService.updatePage$.next(pages[pages.length - 1]),
-    );
-  }
-
   toggleThumbnailsPanel() {
-    this.thumbnailsPanelOpened$.pipe(take(1)).subscribe(opened => {
-      this.thumbnailsPanelOpened$.next(!opened);
-    });
-    this.viscollPanelOpened$.pipe(take(1)).subscribe(opened => {
-      if (opened) {
-        this.viscollPanelOpened$.next(!opened);
-      }
-    });
+    this.toggleThumbnailsPanel$.next();
+    this.toggleViscollPanel$.next(false);
   }
 
   toggleViscollPanel() {
-    this.viscollPanelOpened$.pipe(take(1)).subscribe(opened => {
-      this.viscollPanelOpened$.next(!opened);
-    });
-    this.thumbnailsPanelOpened$.pipe(take(1)).subscribe(opened => {
-      if (opened) {
-        this.thumbnailsPanelOpened$.next(!opened);
-      }
-    });
-  }
-
-  private calculateThumbsPerPage() {
-    const thContainer = this.thumbnailsContainer?.nativeElement;
-    if (thContainer) {
-      const thMaxHeight = parseInt(window.getComputedStyle(document.documentElement).getPropertyValue('--thumbnail-height'), 10);
-      const thMaxWidth = parseInt(window.getComputedStyle(document.documentElement).getPropertyValue('--thumbnail-width'), 10);
-      this.thViewerSettings$.next({
-        col: Math.floor(thContainer.clientWidth / thMaxWidth),
-        row: Math.floor(thContainer.clientHeight / thMaxHeight),
-      });
-      this.cdref.detectChanges();
-    }
+    this.toggleViscollPanel$.next();
+    this.toggleThumbnailsPanel$.next(false);
   }
 }
