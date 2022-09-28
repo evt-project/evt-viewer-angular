d503736ddef0104f780bfd6f88a8411ec664bb75
ChiaraDipi <dipi.chiara@gmail.com>
2022-09-28 09:47:16 +0200

    Add script to prepare build release
    Add user_paths.sh to define paths to folders containing sample xml and config

---
+++ build-release.sh
@@ -0,0 +1,19 @@
+nvm use 12
+
+npm run build -- --c=release  
+
+if [ ! -f ./user_paths.sh ]; then
+    echo "user_paths.sh file not found."
+fi
+
+. ./user_paths.sh
+
+if [ "$XML_DATA_FOLDER" ]; then
+    cp -r $XML_DATA_FOLDER release/assets/data
+    echo "xml data folder copied in dist."
+fi
+
+if [ "$CONFIG_FOLDER" ]; then
+    cp -r $CONFIG_FOLDER release/assets/
+    echo "config folder copied in dist."
+fi
---
+++ user_paths.sh
@@ -0,0 +1,2 @@
+XML_DATA_FOLDER=../evt-sample-documents/EVT3/v.alpha/data
+CONFIG_FOLDER=../evt-sample-documents/EVT3/v.alpha/config
