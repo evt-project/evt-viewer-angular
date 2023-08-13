def4ee339bf4c0721db4e0ca36952476c258ea3f
davivcu <dav.iv.kovac@gmail.com>
2023-08-14 01:57:50 +0200

    console.log commands removed

--- src/app/components/analogues/analogues.component.ts
+++ src/app/components/analogues/analogues.component.ts
@@ -17,7 +17,7 @@ export class AnaloguesComponent implements OnInit {
 
   public getEntries(data) {
     this.analogues = data.flat();
-    console.log('analogues in this page ',this.analogues);
+    //console.log('analogues in this page',this.analogues);
   }
 
   constructor(
