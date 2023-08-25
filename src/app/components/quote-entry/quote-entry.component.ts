4fda25e1f2fcc665a75929e6322079beb3bafd51
davivcu <dav.iv.kovac@gmail.com>
2023-08-25 15:49:54 +0200

    editorial conventions default style for sources and analogues

--- src/app/components/quote-entry/quote-entry.component.ts
+++ src/app/components/quote-entry/quote-entry.component.ts
@@ -1,54 +1,54 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { Subject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 import { scan, startWith } from 'rxjs/operators';
 
-import { EditorialConventionDefaults } from 'src/app/services/editorial-conventions.service';
 import { EditorialConventionLayoutData } from '../../directives/editorial-convention-layout.directive';
 
 import { QuoteEntry } from '../../models/evt-models';
 import { register } from '../../services/component-register.service';
 import { EVTStatusService } from '../../services/evt-status.service';
-import { EditionlevelSusceptible, TextFlowSusceptible } from '../components-mixins';
+import { EditionLevelType } from 'src/app/app.config';
+import { EditionlevelSusceptible, Highlightable } from '../components-mixins';
 
-export interface QuoteEntryComponent extends EditionlevelSusceptible, TextFlowSusceptible { }
-
+export interface QuoteEntryComponent extends EditionlevelSusceptible, Highlightable { } { }
+@register(QuoteEntry)
 @Component({
   selector: 'evt-quote-entry',
   templateUrl: './quote-entry.component.html',
   styleUrls: ['./quote-entry.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-
-@register(QuoteEntry)
-
 export class QuoteEntryComponent {
-
   @Input() data: QuoteEntry;
+
+  private edLevel: EditionLevelType;
+  @Input() set editionLevel(el: EditionLevelType) {
+    this.edLevel = el;
+    this.editionLevelChange.next(el);
+  }
+  get editionLevel() { return this.edLevel; }
+  editionLevelChange = new BehaviorSubject<EditionLevelType | ''>('');
+
+  get editorialConventionData(): EditorialConventionLayoutData {
+    return {
+      name: '.sources',
+      attributes: this.data?.attributes || {},
+      editionLevel: this.editionLevel,
+      defaultsKey: '.sources',
+    };
+  }
 
   public opened = false;
 
   toggleOpened$ = new Subject<boolean | void>();
+
   opened$ = this.toggleOpened$.pipe(
     scan((currentState: boolean, val: boolean | undefined) => val === undefined ? !currentState : val, false),
     startWith(false),
   );
 
-  get editorialConventionData(): EditorialConventionLayoutData {
-    return {
-      name: 'quotation',
-      attributes: this.data?.attributes || {},
-      editionLevel: this.editionLevel,
-      defaultsKey: this.defaultsKey,
-    };
-  }
-
-  get defaultsKey(): EditorialConventionDefaults {
-    return 'quotation';
-  }
-
   toggleAppEntryBox(e: MouseEvent) {
     e.stopPropagation();
-    console.log('ho aperto');
     this.opened = !this.opened;
   }
 
@@ -62,7 +62,6 @@ export class QuoteEntryComponent {
 
   constructor(
     public evtStatusService: EVTStatusService,
-  ) {
-  }
+  ) {}
 
 }
