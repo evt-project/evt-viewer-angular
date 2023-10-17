6be0770ee585b53437f2320c4ff480f214c9326e
davivcu <dav.iv.kovac@gmail.com>
2023-10-17 19:13:15 +0200

    Clean quote entry and analogue entry component, add pointer on mouseover event for quotes and analogues

--- src/app/components/analogues/analogue-entry/analogue-entry.component.ts
+++ src/app/components/analogues/analogue-entry/analogue-entry.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input } from '@angular/core';
-import { combineLatest, Subject } from 'rxjs';
-import { map, scan, startWith } from 'rxjs/operators';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { Subject } from 'rxjs';
+import { scan, startWith } from 'rxjs/operators';
 
 import { EditorialConventionDefaults } from 'src/app/services/editorial-conventions.service';
 import { EditorialConventionLayoutData } from '../../../directives/editorial-convention-layout.directive';
@@ -8,14 +8,15 @@ import { EditorialConventionLayoutData } from '../../../directives/editorial-con
 import { ParallelPassage } from '../../../models/evt-models';
 import { register } from '../../../services/component-register.service';
 import { EVTStatusService } from '../../../services/evt-status.service';
-import { EditionlevelSusceptible, Highlightable, TextFlowSusceptible } from '../../components-mixins';
+import { EditionlevelSusceptible, TextFlowSusceptible } from '../../components-mixins';
 
-export interface AnalogueEntryComponent extends EditionlevelSusceptible, Highlightable, TextFlowSusceptible { }
+export interface AnalogueEntryComponent extends EditionlevelSusceptible, TextFlowSusceptible { }
 
 @Component({
   selector: 'evt-analogue-entry',
   templateUrl: './analogue-entry.component.html',
   styleUrls: ['./analogue-entry.component.scss','../../sources/sources.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 @register(ParallelPassage)
 export class AnalogueEntryComponent {
@@ -28,13 +29,6 @@ export class AnalogueEntryComponent {
   opened$ = this.toggleOpened$.pipe(
     scan((currentState: boolean, val: boolean | undefined) => val === undefined ? !currentState : val, false),
     startWith(false),
-  );
-
-  quoteHighlight$ = combineLatest([
-    this.opened$,
-    this.evtStatusService.currentQuotedId$,
-  ]).pipe(
-    map(([opened, currentId]) => currentId === this.data.id && !opened),
   );
 
   get editorialConventionData(): EditorialConventionLayoutData {
