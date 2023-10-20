e4b23f6c2ad1d59fdd06503e9c0617a31834e997
davivcu <dav.iv.kovac@gmail.com>
2023-10-20 21:00:49 +0200

    Fix various suggested issues by revisors

--- src/app/services/xml-parsers/analogues-entries-parser.service.ts
+++ src/app/services/xml-parsers/analogues-entries-parser.service.ts
@@ -7,14 +7,14 @@ import { Analogue, AnalogueClass, XMLElement } from '../../models/evt-models';
 })
 export class AnalogueEntriesParserService {
 
-  private tagName = `.${AnalogueClass}`;
+  private className = `.${AnalogueClass}`;
   private parserName = 'evt-analogue-entry-parser';
 
   public parseAnaloguesEntries(document: XMLElement) {
 
     const analogueParser = ParserRegister.get(this.parserName);
 
-    return Array.from(document.querySelectorAll<XMLElement>(this.tagName))
+    return Array.from(document.querySelectorAll<XMLElement>(this.className))
       .map((analogue) => analogueParser.parse(analogue) as Analogue);
   }
 
