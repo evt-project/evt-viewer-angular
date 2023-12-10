fa4a24bc2f2bb7f0927eb3c1720f62dfa4152ec7
davivcu <dav.iv.kovac@gmail.com>
2023-12-10 19:05:31 +0100

    Add readings rendering for app entries while in changesView

--- src/app/components/reading/reading.component.ts
+++ src/app/components/reading/reading.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { Reading } from '../../models/evt-models';
 import { register } from '../../services/component-register.service';
 import { Highlightable } from '../components-mixins';
+import { EditionLevelType } from 'src/app/app.config';
 
 @Component({
   selector: 'evt-reading',
@@ -11,5 +12,6 @@ import { Highlightable } from '../components-mixins';
 @register(Reading)
 export class ReadingComponent extends Highlightable {
   @Input() data: Reading;
+  @Input() editionLevel: EditionLevelType;
   @Input() withDeletions: boolean;
 }
